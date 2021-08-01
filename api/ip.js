9825c719cafe797782f8a6f0e32e063722400778
Dylan Frankland <dfrankland@users.noreply.github.com>
2021-08-01 06:36:24 -0700

    Update ip.js

--- api/ip.js
+++ api/ip.js
@@ -1,8 +1,10 @@
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   // https://vercel.com/docs/edge-network/headers#x-forwarded-for
   const ip = req.headers['x-forwarded-for'];
 
   res.json({ ip });
+  
+  await new Promise(resolve => setTimeout(resolve, 200));
 
   throw new Error(ip);
 };
